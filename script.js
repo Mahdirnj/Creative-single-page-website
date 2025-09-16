2ee6b74abf834d6d758be3cdf20b5a41bbc46577
Mahdi <mahdiranjkesh16@gmail.com>
2025-09-16 13:15:02 +0330

    Refactor HTML structure, enhance navbar, and improve styling for a creative single page website

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-    const $animation_elements = $('.section-title, .card');
-    const $window = $(window);
+const $animation_elements = $('.section-title, .card');
+const $window = $(window);
 
-    function check_if_in_view() {
-        const window_height = $window.height();
-        const window_top_position = $window.scrollTop();
-        const window_bottom_position = (window_top_position + window_height);
+function check_if_in_view() {
+    const window_height = $window.height();
+    const window_top_position = $window.scrollTop();
+    const window_bottom_position = (window_top_position + window_height);
 
-        $.each($animation_elements, function () {
-            const $element = $(this);
-            const element_height = $element.outerHeight();
-            const element_top_position = $element.offset().top;
-            const element_bottom_position = (element_top_position + element_height);
+    $.each($animation_elements, function () {
+        const $element = $(this);
+        const element_height = $element.outerHeight();
+        const element_top_position = $element.offset().top;
+        const element_bottom_position = (element_top_position + element_height);
 
-            if ((element_bottom_position >= window_top_position) &&
-                (element_top_position <= window_bottom_position)) {
-                $element.addClass('in-view');
-            }
-        });
-    }
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        }
+    });
+}
 
-    $window.on('scroll resize', check_if_in_view);
-    $window.trigger('scroll');
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
 
-$(document).ready(function() {
+$(function () {
     const $navbar = $('#navbar');
-    const $window = $(window);
     const $document = $(document);
     let lastScrollTop = 0;
 
-    $window.on('scroll', function() {
+    // Navbar hide on scroll down, show on scroll up
+    $window.on('scroll', function () {
         const scrollTop = $window.scrollTop();
 
-        if (scrollTop > lastScrollTop && scrollTop > 50) {
+        if (scrollTop > lastScrollTop && scrollTop > 80) {
             $navbar.addClass('fade-out');
         } else {
             if (scrollTop + $window.height() < $document.height()) {
@@ -40,5 +40,22 @@ $(document).ready(function() {
         }
 
         lastScrollTop = scrollTop;
+    });
+
+    // Smooth scroll for internal links
+    $('a[href^="#"]').on('click', function (e) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70
+            }, 600);
+
+            // Collapse responsive menu when a link is clicked
+            const bsCollapse = $('.navbar-collapse');
+            if (bsCollapse.hasClass('show')) {
+                $('.navbar-toggler').trigger('click');
+            }
+        }
     });
 });
